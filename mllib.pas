4a43e9996fad17395db1dddc79a4c0cd8c6e132a
Magno Lima <lima.magno@gmail.com>
2022-02-25 08:53:09 -0300

    Update mllib.pas
    Adicionado outras funções

--- mllib.pas
+++ mllib.pas
@@ -8,6 +8,8 @@ uses
 interface
 
 function DistanciaGrauToMetro(Const x1, y1, x2, y2: single; LongaDistancia: boolean = False): single;
+function DecodeString(Const Text: string; StartKey, MultKey, AddKey: integer): string;
+function EncodeString(Const Text: string; StartKey, MultKey, AddKey: integer): string;
 
 implentation
 
@@ -27,6 +29,30 @@ begin
   fLatitudeRadiano := fGrauRadiano * ((y1 + y2) / 2);
   fDistancia := sqrt(power((x1 - x2), 2) + power((y1 - y2), 2)) * cos(fLatitudeRadiano) * 111319.49166666666666666666666667;
   Result := round(fDistancia);
+end;
+
+function EncodeString(Const Text: string; StartKey, MultKey, AddKey: integer): string;
+var
+  i: Word;
+begin
+  Result := '';
+  for i := Low(Text) to High(Text) do
+  begin
+    Result := Result + WideChar(Ord(Text[i]) xor (StartKey shr 8));
+    StartKey := (Ord(Result[i]) + StartKey) * MultKey + AddKey;
+  end;
+end;
+
+function DecodeString(Const Text: string; StartKey, MultKey, AddKey: integer): string;
+var
+  i: Word;
+begin
+  Result := '';
+  for i := Low(Text) to High(Text) do
+  begin
+    Result := Result + WideChar(Ord(Text[i]) xor (StartKey shr 8));
+    StartKey := (Ord(Text[i]) + StartKey) * MultKey + AddKey;
+  end;
 end;
 
 end.
